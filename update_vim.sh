9d1f5b7e234c07a844038987f69b3a6835f2811e
MLSTT <marialsottomayor@gmail.com>
2021-06-08 15:27:38 +0100

    Auto-update vimrc - update

--- update_vim.sh
+++ update_vim.sh
@@ -6,16 +6,18 @@
 #    By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/14 13:58:23 by mlanca-c          #+#    #+#              #
-#    Updated: 2021/05/14 14:03:54 by mlanca-c         ###   ########.fr        #
+#    Updated: 2021/06/08 15:27:21 by mlanca-c         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 clear
 echo "Copying .vimrc to ~/.vimrc"
 cp -f .vimrc ~/.
+sleep 2
+
 clear
 echo "Updating git folder"
 git status
-git add .vimrc
-git commit -m "Auto-update vimrc - update"
+git commit -am "Auto-update vimrc - update"
 git push
+sleep 2
